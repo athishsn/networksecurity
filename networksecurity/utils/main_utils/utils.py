caf9295cec4b92c0947f45f99e0b1634cbc02e69
athishsn <athishsn@gmail.com>
2025-03-27 15:34:01 +0530

    Data transformation

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -25,3 +25,25 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(file_path:str, array:np.array):
+    """save numpy array data to a file """
+    
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def save_object(file_path: str, obj: object)-> None:
+    
+    try:
+        logging.info("Entered the save object method of utils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save-obj method od utils class")        
+    except Exception as e:
+        raise  NetworkSecurityException(e, sys)
